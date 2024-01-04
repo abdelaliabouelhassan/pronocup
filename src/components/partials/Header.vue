63bfb89cba08789b6f2ed20cc22cabefd1feb275
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-05 00:22:45 +0100

    header mobile

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class=" w-full py-4 px-4 lg:px-2 xl:px-0">
-        <div class=" w-full max-w-[1269px] mx-auto flex items-center justify-between">
+    <div class=" w-full py-4 px-4 lg:px-2 xl:px-0 h-[120px] flex border-[#F2F3F6] dark:border-gray-500 lg:border-b-0" :class="{'border-b-2':show}">
+        <div class=" w-full max-w-[1269px] mx-auto flex m-auto items-center justify-between">
             <a href="/">
-                <img src="/images/logo_black.png" class=" w-full dark:hidden  " alt="">
-                 <img src="/images/logo_white.png" class=" w-full hidden dark:block " alt="">
+                <img src="/images/logo_black.png" class="  dark:hidden  " alt="">
+                 <img src="/images/logo_white.png" class="  hidden dark:block " alt="">
             </a>
 
             <div class=" flex items-center space-x-5 lg:space-x-5 xl:space-x-10">
@@ -12,7 +12,7 @@
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">Nos clients</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">FAQ</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">Contact</a>
-                <div>
+                <div class=" hidden sm:block">
                     <button class=" flex items-center space-x-3 bg-primary dark:bg-white rounded-[2rem] border-2 border-[#242726] text-white dark:text-primary text-base font-bold h-[3rem] px-4 hover:scale-110 duration-200 font-helvetica ">
                         <span>Aller vers l’app web</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class=" dark:hidden" >
@@ -25,7 +25,7 @@
                         </svg>
                     </button>
                 </div>
-                <button class=" flex items-center space-x-2 lg:hidden">
+                <button class=" flex items-center space-x-2 lg:hidden" @click="show = !show">
                     <span class=" text-primary dark:text-white text-[0.86044rem] font-bold font-helvetica">
                         Menu
                     </span>
@@ -43,12 +43,68 @@
             </div>
         </div>
     </div>
+
+     <aside id="default-sidebar" :class="{'translate-x-0  sideBar':show,'translate-x-full ':!show}" class="  fixed py-4  bg-white dark:bg-primary  lg:hidden top-[7.5rem] h-[89.8%] right-0 z-40 w-full sm:max-w-[75%]  transition-transform  " aria-label="Sidebar">
+            <div class="h-full  overflow-y-auto flex flex-col items-center justify-between pb-16 sm:pb-10  ">
+                <div class=" w-full flex">
+                     <ul class="space-y-8 text-center font-medium py-8 m-auto">
+                    <li>
+                     <nuxt-link to="/" class=" text-primary dark:text-white font-helvetica font-bold text-base ">
+                            Le concept
+                     </nuxt-link>
+                    </li>
+                    <li>
+                    <nuxt-link to="/" class=" text-primary dark:text-white font-helvetica font-bold text-base ">
+                           La prochaine compétition
+                     </nuxt-link>
+
+                    </li>
+                    <li>
+                    <nuxt-link to="/" class=" text-primary dark:text-white font-helvetica font-bold text-base ">
+                            Nos clients
+                     </nuxt-link>
+
+                    </li>
+                     <li>
+                    <nuxt-link to="/" class=" text-primary dark:text-white font-helvetica font-bold text-base ">
+                            FAQ
+                     </nuxt-link>
+
+                    </li>
+                     <li>
+                    <nuxt-link to="/" class=" text-primary dark:text-white font-helvetica font-bold text-base ">
+                            Contact
+                     </nuxt-link>
+
+                    </li>
+                </ul>
+                </div>
+
+                <div class=" w-full   max-w-[20rem] flex flex-col items-start space-y-4">
+                    <button class=" w-full rounded-[2.58631rem] bg-white space-x-2 hover:scale-110 duration-200 h-[3.70rem] flex items-center justify-center border-[#EFEFEF] border-[2.5px]">
+                        <img src="/images/apple.png" alt="">
+                        <span class=" text-primary font-bold font-helvetica text-[1.21706rem]">Télécharger sur l’app store</span>
+                    </button>
+                     <button class=" w-full rounded-[2.58631rem] bg-white space-x-2 hover:scale-110 duration-200 h-[3.70rem] flex items-center justify-center border-[#EFEFEF] border-[2.5px]">
+                        <img src="/images/googleplay.png" alt="">
+                        <span class=" text-primary font-bold font-helvetica text-[1.21706rem]">Télécharger sur google play</span>
+                    </button>
+                </div>  
+            </div>
+     </aside>
 </template>
 
 <script setup>
-    import { onMounted } from "vue";
-
+    import { onMounted, ref } from "vue";
+    const show = ref(false)
     onMounted(() => {
-            document.body.classList.add('dark');
+             document.body.classList.add('dark');
     })
 </script>
+
+
+<style scoped>
+ .sideBar {
+    box-shadow: -12px 34px 35px 0px rgba(0, 0, 0, 0.25);
+ }
+</style>
