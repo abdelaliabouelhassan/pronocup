8977250d2b4846fcc49d8da7d1434925182d3dfe
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-05 16:02:24 +0100

    dark mode

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -6,14 +6,14 @@
                  <img src="/images/logo_white.png" class="  hidden dark:block " alt="">
             </a>
 
-            <div class=" flex items-center space-x-5 lg:space-x-5 xl:space-x-10">
+            <div class=" flex items-center space-x-5 lg:space-x-5 xl:space-x-10 whitespace-nowrap">
                 <a href="" class=" text-primary dark:text-white font-helvetica font-bold text-base hidden lg:block">Le concept</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">La prochaine compétition</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">Nos clients</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">FAQ</a>
                 <a href="" class=" text-primary dark:text-white  font-helvetica font-bold text-base hidden lg:block">Contact</a>
                 <div class=" hidden sm:block">
-                    <button @click="toggleDarkMode" class=" flex items-center space-x-3 bg-primary dark:bg-white rounded-[2rem] border-2 border-[#242726] text-white dark:text-primary text-base font-bold h-[3rem] px-4 hover:scale-110 duration-200 font-helvetica ">
+                    <button class=" flex items-center space-x-3 bg-primary dark:bg-white rounded-[2rem] border-2 border-[#242726] text-white dark:text-primary text-base font-bold h-[3rem] px-4 hover:scale-110 duration-200 font-helvetica ">
                         <span>Aller vers l’app web</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class=" dark:hidden" >
                             <path d="M7 17L17 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -38,6 +38,15 @@
                         <path d="M2.72095 10.4998H18.2093" stroke="white" stroke-width="1.72093" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M2.72095 5.33691H18.2093" stroke="white" stroke-width="1.72093" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M2.72095 15.6626H18.2093" stroke="white" stroke-width="1.72093" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+
+                <button  @click="toggleDarkMode"  class=" dark:text-white text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 dark:block hidden">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 dark:hidden">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                     </svg>
                 </button>
             </div>
