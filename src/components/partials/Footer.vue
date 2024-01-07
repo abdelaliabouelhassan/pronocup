0c23038c299e106bd80ea72c37ced7e721647a71
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-07 15:11:33 +0100

    footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,3 +1,101 @@
 <template>
-    footer
+    <footer class=" w-full max-w-[1280px] mx-auto space-y-5 py-10 px-4 xl:px-0">
+        <div class=" w-full bg-primary rounded-[2rem] px-4 xl:px-20 py-10 lg:py-20 flex flex-col items-start space-y-10">
+            <div class=" w-[72px] h-[72px] rounded-full bg-[#28D9BB] flex">
+                <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none" class=" m-auto">
+                    <path d="M34.4928 19H25.197L22.0984 23.6479H15.9012L12.8026 19H3.50684" stroke="#171717" stroke-width="3.09859" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M8.85191 8.32519L3.50684 18.9998V28.2956C3.50684 29.1174 3.83329 29.9055 4.41439 30.4866C4.99549 31.0677 5.78363 31.3942 6.60543 31.3942H31.3942C32.216 31.3942 33.0041 31.0677 33.5852 30.4866C34.1663 29.9055 34.4928 29.1174 34.4928 28.2956V18.9998L29.1477 8.32519C28.8912 7.80894 28.4957 7.37449 28.0058 7.07069C27.5159 6.76689 26.9509 6.60577 26.3744 6.60547H11.6251C11.0487 6.60577 10.4837 6.76689 9.99381 7.07069C9.50389 7.37449 9.10843 7.80894 8.85191 8.32519V8.32519Z" stroke="#171717" stroke-width="3.09859" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+
+            <div class=" w-full flex lg:flex-row flex-col items-start space-y-10 lg:space-y-0 lg:items-end lg:first-letter:justify-between">
+                <div class=" flex items-start flex-col space-y-4 max-w-[40rem]">
+                    <h1 class=" text-white font-helvetica font-medium text-[1.75rem] md:text-[4rem] leading-[116.865%]">Restez informé de notre actualité</h1>
+                    <p class=" text-[#98A2B3] font-normal font-helvetica text-xl leading-[125.685%]">
+                        Inscrivez-vous à notre newsletter pour ne rien rater de nos  mises à jour !
+                    </p>
+                </div>
+
+                <div class=" flex flex-col items-start space-y-4 max-w-[28rem] w-full">
+                    <h2 class=" text-white text-base font-helvetica font-normal">Je veux être informé</h2>
+                    <div class=" flex sm:items-center  sm:flex-row flex-col items-start space-y-4 sm:space-y-0 sm:space-x-4 w-full">
+                        <input type="text" class=" w-full sm:max-w-[18.75rem] h-[3.3125rem] rounded-[2rem] bg-[#ffffff1a] pr-4 pl-8 outline-none text-white font-helvetica text-base font-normal placeholder-white" placeholder="Entrez votre adresse e-mail">
+                        <button class=" bg-[#28D9BB] rounded-[2rem] text-center h-[3.3125rem] sm:max-w-[7.5rem] w-full hover:scale-110 duration-200 text-primary font-helvetica font-bold text-[0.89713rem]">
+                            Je m’inscris
+                        </button>
+                    </div>
+                    <p class=" text-[#98A2B3] text-base font-helvetica font-normal">
+                        En vous inscrivant vous acceptez notre <span class=" text-white">Politique de confidentialité </span>
+                    </p>
+                </div>
+            </div>
+            <div class=" w-full h-[0.125rem] bg-opacity-50 bg-[#98A2B3]"></div>
+
+            <div class=" w-full flex md:flex-row flex-col  space-y-8 md:space-y-0 items-start md:justify-between">
+                <div class=" w-full max-w-[14.6875rem] flex flex-col items-start space-y-4">
+                    <div class=" w-full">
+                        <img src="/images/logo_white.png" class=" w-full h-full object-cover" alt="">
+                    </div>
+                    <span class=" text-[#98A2B3] font-helvetica font-normal text-base text-opacity-60">L’application ultime de pronostics</span>
+                </div>
+                <div class=" flex flex-row items-center space-x-4 md:flex-col md:items-start md:space-y-4">
+                    <span class=" text-white font-helvetica text-base font-normal">Pronocup</span>
+                    <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">lorem</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">lorem</a>
+                      <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">lorem</a>
+                </div>
+                 <div class=" flex flex-row items-center space-x-4 md:flex-col md:items-start md:space-y-4">
+                    <span class=" text-white font-helvetica text-base font-normal">L’app</span>
+                    <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Mobile</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Web</a>
+                </div>
+                 <div class=" flex flex-row items-center space-x-4 md:flex-col md:items-start md:space-y-4">
+                    <span class=" text-white font-helvetica text-base font-normal">L’entreprise</span>
+                    <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">À propos</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">lorem</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">lorem</a>
+                </div>
+                 <div class=" flex flex-col items-start space-y-4">
+                    <span class=" text-white font-helvetica text-base font-normal">Support</span>
+                    <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Aide</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">FAQ</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Contact</a>
+                </div>
+                 <div class=" flex flex-col items-start space-y-4">
+                    <span class=" text-white font-helvetica text-base font-normal">Mentions légales</span>
+                    <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Politique de  confidentialité</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Condition d’utilisation</a>
+                     <a href="#" class=" text-opacity-60 text-base font-helvetica font-normal text-[#98A2B3] hover:text-opacity-100 duration-200">Cookies</a>
+                </div>
+            </div>
+        </div>
+
+        <div class=" w-full flex items-center justify-between">
+            <span class=" text-primary dark:text-white text-base font-helvetica font-normal">© 2023 Pronocup. </span>
+            <div class=" flex items-center space-x-2">
+                <a href="#" class="w-[2.75rem] h-[2.75rem] rounded-full flex bg-[#28D9BB]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class=" text-primary m-auto" viewBox="0 0 16 16">
+                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                    </svg>
+                </a>
+                <a href="#" class="w-[2.75rem] h-[2.75rem] rounded-full flex bg-[#28D9BB]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class=" m-auto">
+                      <path d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z" fill="#25282B"/>
+                    </svg>
+                </a>
+                 <a href="#" class="w-[2.75rem] h-[2.75rem] rounded-full flex bg-[#28D9BB]">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class=" m-auto">
+                     <path d="M23 2.99931C22.0424 3.67479 20.9821 4.19142 19.86 4.52931C19.2577 3.83682 18.4573 3.34601 17.567 3.12324C16.6767 2.90047 15.7395 2.95651 14.8821 3.28377C14.0247 3.61102 13.2884 4.19371 12.773 4.95303C12.2575 5.71235 11.9877 6.61165 12 7.52931V8.52931C10.2426 8.57488 8.50127 8.18513 6.93101 7.39476C5.36074 6.60439 4.01032 5.43795 3 3.99931C3 3.99931 -1 12.9993 8 16.9993C5.94053 18.3973 3.48716 19.0982 1 18.9993C10 23.9993 21 18.9993 21 7.49931C20.9991 7.22077 20.9723 6.94291 20.92 6.66931C21.9406 5.66281 22.6608 4.39203 23 2.99931Z" fill="#25282B"/>
+                   </svg>
+                </a>
+                 <a href="#" class="w-[2.75rem] h-[2.75rem] rounded-full flex bg-[#28D9BB]">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class=" m-auto">
+                        <path d="M16 8.00098C17.5913 8.00098 19.1174 8.63312 20.2426 9.75834C21.3679 10.8836 22 12.4097 22 14.001V21.001H18V14.001C18 13.4705 17.7893 12.9618 17.4142 12.5868C17.0391 12.2117 16.5304 12.001 16 12.001C15.4696 12.001 14.9609 12.2117 14.5858 12.5868C14.2107 12.9618 14 13.4705 14 14.001V21.001H10V14.001C10 12.4097 10.6321 10.8836 11.7574 9.75834C12.8826 8.63312 14.4087 8.00098 16 8.00098Z" fill="#25282B"/>
+                        <path d="M6 9.00098H2V21.001H6V9.00098Z" fill="#25282B"/>
+                        <path d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z" fill="#25282B"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </footer>
 </template>
