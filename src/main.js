6b49b96c49e4ed5982c319da55cb235f20ef5835
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-09 23:58:11 +0100

    span animation

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import  animate  from './plugins /animate.js'
 import  animateleft  from './plugins /animateleft.js'
 import animateright from './plugins /animateright.js'
+import accordion from './plugins /accordion.js'
 
 const app = createApp(App);
 
 app.directive('animate', animate);
 app.directive('animateleft', animateleft);
 app.directive('animateright', animateright);
+app.directive('accordion', accordion);
 
 app.mount('#app');
